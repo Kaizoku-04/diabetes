8fc58266e40e5a7f2c6bb72ef42170998f938459
Kaizoku-04 <dodorapapah@gmail.com>
2025-03-17 01:23:17 +0300

    Update Firebase initialization to use Streamlit secrets

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if not firebase_admin._apps:
         "type": st.secrets.firebase.type,
         "project_id": st.secrets.firebase.project_id,
         "private_key_id": st.secrets.firebase.private_key_id,
-        "private_key": st.secrets.firebase.private_key.replace('\\n', '\n'),
+        "private_key": st.secrets.firebase.private_key,
         "client_email": st.secrets.firebase.client_email,
         "client_id": st.secrets.firebase.client_id,
         "auth_uri": st.secrets.firebase.auth_uri,
@@ -20,7 +20,7 @@ if not firebase_admin._apps:
         "auth_provider_x509_cert_url": st.secrets.firebase.auth_provider_x509_cert_url,
         "client_x509_cert_url": st.secrets.firebase.client_x509_cert_url
     }
-    cred = credentials.Certificate("firebase_cred")  # Download from Firebase Console
+    cred = credentials.Certificate(firebase_cred)  # Download from Firebase Console
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
